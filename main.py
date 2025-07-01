0a22bf3bb41f6f8b2dc4da4e5ec47b88be845724
sharmahardik0712 <113001937+sharmahardik0712@users.noreply.github.com>
2025-07-01 13:10:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,3 +96,7 @@ def get_random_word_old():
 @app.get("/word")
 def get_word():
     return random_word()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
